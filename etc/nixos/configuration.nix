9ae3c555e0d357493ff63aa07fe1c0ec39fda491
Dustin Krysak <dustin@bashfulrobot.com>
2023-02-13 14:10:16 -0800

    feat: :sparkles: added call to applications.nix Changes to be committed: 	modified:   etc/nixos/configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -15,6 +15,9 @@
     # Pantheon configuration
     ./pantheon-desktop.nix
 
+    # Application Installations
+    ./applications.nix
+
   ];
 
   ##### Bootloader configuration
@@ -112,42 +115,6 @@
 
   # Enable Bluetooth
   hardware.bluetooth.enable = true;
-
-  ##### System Packages configuration
-
-  # Allow Unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # Allow GTK apps to use dconf for settings
-  programs.dconf.enable = true;
-
-  # Install system level packages
-  environment.systemPackages = with pkgs; [
-    wget
-    google-chrome
-    vscode
-    ripgrep
-    fd
-    curl
-    git
-    neovim
-    _1password
-    _1password-gui
-    oh-my-posh
-    meslo-lgs-nf
-    espanso
-    zsh
-    tilix
-    gnumake
-    nix-index
-    bat
-    exa
-    dua
-    sd
-    du-dust
-    restic
-    autorestic
-  ];
 
   # services.avahi = {
   #   enable = true;
